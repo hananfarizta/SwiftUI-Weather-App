d792d4a1834eb3c564ecf00d030becbddc50bcef
hananfarizta <hananfariztaa@gmail.com>
2024-06-25 10:58:45 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Hanan Farizta on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
